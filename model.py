66035ac75192da1f4f694592f65a7e6a4fa77aa0
Sapil <sarahpileroudi@gmai.com>
2020-04-07 02:05:24 -0700

    first commit

---
+++ model.py
@@ -0,0 +1,57 @@
+import torch.nn as nn
+import torch.nn.functional as F
+TOTAL_CLASSES = 100
+
+class BaseNet(nn.Module):
+    def __init__(self):
+        super(BaseNet, self).__init__()
+        
+        # <<TODO#3>> Add more conv layers with increasing 
+        # output channels
+        # <<TODO#4>> Add normalization layers after conv
+        # layers (nn.BatchNorm2d)
+
+        # Also experiment with kernel size in conv2d layers (say 3
+        # inspired from VGGNet)
+        # To keep it simple, keep the same kernel size
+        # (right now set to 5) in all conv layers.
+        # Do not have a maxpool layer after every conv layer in your
+        # deeper network as it leads to too much loss of information.
+
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+
+        # <<TODO#3>> Add more linear (fc) layers
+        # <<TODO#4>> Add normalization layers after linear and
+        # experiment inserting them before or after ReLU (nn.BatchNorm1d)
+        # More on nn.sequential:
+        # http://pytorch.org/docs/master/nn.html#torch.nn.Sequential
+        
+        self.fc_net = nn.Sequential(
+            nn.Linear(16 * 5 * 5, TOTAL_CLASSES//2),
+            nn.ReLU(inplace=True),
+            nn.Linear(TOTAL_CLASSES//2, TOTAL_CLASSES),
+        )
+
+    def forward(self, x):
+
+        # <<TODO#3&#4>> Based on the above edits, you'll have
+        # to edit the forward pass description here.
+
+        x = self.pool(F.relu(self.conv1(x)))
+        # Output size = 28//2 x 28//2 = 14 x 14
+
+        x = self.pool(F.relu(self.conv2(x)))
+        # Output size = 10//2 x 10//2 = 5 x 5
+
+        # See the CS231 link to understand why this is 16*5*5!
+        # This will help you design your own deeper network
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.fc_net(x)
+
+        # No softmax is needed as the loss function in step 3
+        # takes care of that
+        
+        return x
+
---
+++ train.py
@@ -0,0 +1,213 @@
+"""Headers"""
+
+from __future__ import print_function
+from PIL import Image
+import os
+import os.path
+import numpy as np
+import sys
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
+
+import torch.utils.data as data
+from torchvision.datasets.utils import download_url, check_integrity
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os.path
+import sys
+import torch
+import torch.utils.data
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from model import BaseNet
+from dataloader import CIFAR100_SFU_CV
+
+criterion = nn.CrossEntropyLoss()
+
+
+np.random.seed(111)
+torch.cuda.manual_seed_all(111)
+torch.manual_seed(111)
+
+# <<TODO#5>> Based on the val set performance, decide how many
+# epochs are apt for your model.
+# ---------
+EPOCHS = 1
+# ---------
+
+IS_GPU = True
+TEST_BS = 256
+TOTAL_CLASSES = 100
+TRAIN_BS = 32
+PATH_TO_CIFAR100_SFU_CV = "/data/"
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# Using transforms.ToTensor(), transform them to Tensors of normalized range
+# [-1, 1].
+
+
+# <<TODO#1>> Use transforms.Normalize() with the right parameters to 
+# make the data well conditioned (zero mean, std dev=1) for improved training.
+# <<TODO#2>> Try using transforms.RandomCrop() and/or transforms.RandomHorizontalFlip()
+# to augment training data.
+# After your edits, make sure that test_transform should have the same data
+# normalization parameters as train_transform
+# You shouldn't have any data augmentation in test_transform (val or test data is never augmented).
+# ---------------------
+
+train_transform = transforms.Compose(
+    [transforms.RandomCrop(20),
+     transforms.Resize((32,32)),
+     transforms.RandomHorizontalFlip(p=0.5),
+     transforms.ToTensor(),
+     transforms.Normalize((0.0,),(1.0,))]
+    )
+test_transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.0,),(1.0,))])
+# ---------------------
+
+trainset = CIFAR100_SFU_CV(root=PATH_TO_CIFAR100_SFU_CV, fold="train",
+                                        download=True, transform=train_transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=TRAIN_BS,
+                                          shuffle=True, num_workers=2)
+print("Train set size: "+str(len(trainset)))
+
+valset = CIFAR100_SFU_CV(root=PATH_TO_CIFAR100_SFU_CV, fold="val",
+                                       download=True, transform=test_transform)
+valloader = torch.utils.data.DataLoader(valset, batch_size=TEST_BS,
+                                         shuffle=False, num_workers=2)
+print("Val set size: "+str(len(valset)))
+
+testset = CIFAR100_SFU_CV(root=PATH_TO_CIFAR100_SFU_CV, fold="test",
+                                       download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=TEST_BS,
+                                         shuffle=False, num_workers=2)
+print("Test set size: "+str(len(testset)))
+
+# The 100 classes for CIFAR100
+classes = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+
+
+optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9)
+
+plt.ioff()
+fig = plt.figure()
+train_loss_over_epochs = []
+val_accuracy_over_epochs = []
+
+#Create an instance of the nn.module class defined above:
+net = BaseNet()
+
+# For training on GPU, we need to transfer net and data onto the GPU
+# http://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#training-on-gpu
+if IS_GPU:
+    net = net.cuda()
+
+
+
+for epoch in range(EPOCHS):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        if IS_GPU:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+    
+    # Normalizing the loss by the total number of train batches
+    running_loss/=len(trainloader)
+    print('[%d] loss: %.3f' %
+          (epoch + 1, running_loss))
+
+    # Scale of 0.0 to 100.0
+    # Calculate validation set accuracy of the existing model
+    val_accuracy, val_classwise_accuracy = \
+        calculate_val_accuracy(valloader, IS_GPU)
+    print('Accuracy of the network on the val images: %d %%' % (val_accuracy))
+
+    # # Optionally print classwise accuracies
+    # for c_i in range(TOTAL_CLASSES):
+    #     print('Accuracy of %5s : %2d %%' % (
+    #         classes[c_i], 100 * val_classwise_accuracy[c_i]))
+
+    train_loss_over_epochs.append(running_loss)
+    val_accuracy_over_epochs.append(val_accuracy)
+
+plt.subplot(2, 1, 1)
+plt.ylabel('Train loss')
+plt.plot(np.arange(EPOCHS), train_loss_over_epochs, 'k-')
+plt.title('train loss and val accuracy')
+plt.xticks(np.arange(EPOCHS, dtype=int))
+plt.grid(True)
+
+plt.subplot(2, 1, 2)
+plt.plot(np.arange(EPOCHS), val_accuracy_over_epochs, 'b-')
+plt.ylabel('Val accuracy')
+plt.xlabel('Epochs')
+plt.xticks(np.arange(EPOCHS, dtype=int))
+plt.grid(True)
+plt.savefig("plot.png")
+plt.close(fig)
+print('Finished Training')
+# -------------
+def calculate_val_accuracy(valloader, is_gpu):
+    """ Util function to calculate val set accuracy,
+    both overall and per class accuracy
+    Args:
+        valloader (torch.utils.data.DataLoader): val set 
+        is_gpu (bool): whether to run on GPU
+    Returns:
+        tuple: (overall accuracy, class level accuracy)
+    """    
+    correct = 0.
+    total = 0.
+    predictions = []
+
+    class_correct = list(0. for i in range(TOTAL_CLASSES))
+    class_total = list(0. for i in range(TOTAL_CLASSES))
+
+    for data in valloader:
+        images, labels = data
+        if is_gpu:
+            images = images.cuda()
+            labels = labels.cuda()
+        outputs = net(Variable(images))
+        _, predicted = torch.max(outputs.data, 1)
+        predictions.extend(list(predicted.cpu().numpy()))
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+        c = (predicted == labels).squeeze()
+        for i in range(len(labels)):
+            label = labels[i]
+            class_correct[label] += c[i]
+            class_total[label] += 1
+
+    class_accuracy = 100 * np.divide(class_correct, class_total)
+    return 100*correct/total, class_accuracy
