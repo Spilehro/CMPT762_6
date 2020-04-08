2f2c84ec29f4e4cb1a08d9dc3c5711b60fee5a39
spilehro <spilehro@blu9402u-b10.csil.sfu.ca>
2020-04-07 18:04:09 -0700

    test script added

--- model.py
+++ model.py
@@ -22,9 +22,24 @@ class BaseNet(nn.Module):
         # Do not have a maxpool layer after every conv layer in your
         # deeper network as it leads to too much loss of information.
 
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv1 = nn.Conv2d(3, 6, 3,padding=1)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.pool = nn.MaxPool2d(2,2)
+
+        self.conv2 = nn.Conv2d(6, 16,3,padding=1 )
+        self.bn2 = nn.BatchNorm2d(16)
+
+        self.conv3 = nn.Conv2d(16, 32, 3,padding=1)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        self.conv4 = nn.Conv2d(32, 64, 3,padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+
+        self.conv5 = nn.Conv2d(64, 128, 3,padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+
+        self.conv6 = nn.Conv2d(128, 256, 3,padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
 
         # <<TODO#3>> Add more linear (fc) layers
         # <<TODO#4>> Add normalization layers after linear and
@@ -33,9 +48,15 @@ class BaseNet(nn.Module):
         # http://pytorch.org/docs/master/nn.html#torch.nn.Sequential
         
         self.fc_net = nn.Sequential(
-            nn.Linear(16 * 5 * 5, TOTAL_CLASSES//2),
+            nn.Linear( 256* 4 * 4, TOTAL_CLASSES//8),
+            nn.ReLU(inplace=True),
+            nn.Linear(TOTAL_CLASSES//8, TOTAL_CLASSES//4),
+            nn.ReLU(inplace=True),
+            nn.Linear(TOTAL_CLASSES//4, TOTAL_CLASSES//2),
             nn.ReLU(inplace=True),
             nn.Linear(TOTAL_CLASSES//2, TOTAL_CLASSES),
+            nn.ReLU(inplace= True)
+
         )
 
     def forward(self, x):
@@ -43,15 +64,25 @@ class BaseNet(nn.Module):
         # <<TODO#3&#4>> Based on the above edits, you'll have
         # to edit the forward pass description here.
 
-        x = self.pool(F.relu(self.conv1(x)))
+        x = F.relu(self.bn1(self.conv1(x)))
         # Output size = 28//2 x 28//2 = 14 x 14
 
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
         # Output size = 10//2 x 10//2 = 5 x 5
+        x=self.pool(x)
+
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = self.pool(x)
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        x = self.pool(x)
+        channels = x.shape[1]
+        w_h = x.shape[2]
 
         # See the CS231 link to understand why this is 16*5*5!
         # This will help you design your own deeper network
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, channels * w_h * w_h)
         x = self.fc_net(x)
 
         # No softmax is needed as the loss function in step 3
---
+++ test.py
@@ -0,0 +1,109 @@
+"""Headers"""
+
+from __future__ import print_function
+from PIL import Image
+import os
+import os.path
+import numpy as np
+import sys
+if sys.version_info[0] == 2:
+    import cPickle as pickle
+else:
+    import pickle
+
+import torch.utils.data as data
+from torchvision.datasets.utils import download_url, check_integrity
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os.path
+import sys
+import torch
+import torch.utils.data
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from model import BaseNet
+from dataloader import CIFAR100_SFU_CV
+import time
+# save numpy array as csv file
+
+
+
+np.random.seed(111)
+torch.cuda.manual_seed_all(111)
+torch.manual_seed(111)
+
+# <<TODO#5>> Based on the val set performance, decide how many
+# epochs are apt for your model.
+# ---------
+EPOCHS = 100
+# ---------
+
+IS_GPU = True
+TEST_BS = 256
+TOTAL_CLASSES = 100
+PATH_TO_CIFAR100_SFU_CV = "../data/cifar100"
+plot_root = 'plots/'
+checkpoints_root = 'checkPoints'
+
+test_transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.0,),(1.0,))])
+# ---------------------
+
+
+testset = CIFAR100_SFU_CV(root=PATH_TO_CIFAR100_SFU_CV, fold="test",
+                                       download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=TEST_BS,
+                                         shuffle=False, num_workers=2)
+print("Test set size: "+str(len(testset)))
+
+# The 100 classes for CIFAR100
+classes = ['apple', 'aquarium_fish', 'baby', 'bear', 'beaver', 'bed', 'bee', 'beetle', 'bicycle', 'bottle', 'bowl', 'boy', 'bridge', 'bus', 'butterfly', 'camel', 'can', 'castle', 'caterpillar', 'cattle', 'chair', 'chimpanzee', 'clock', 'cloud', 'cockroach', 'couch', 'crab', 'crocodile', 'cup', 'dinosaur', 'dolphin', 'elephant', 'flatfish', 'forest', 'fox', 'girl', 'hamster', 'house', 'kangaroo', 'keyboard', 'lamp', 'lawn_mower', 'leopard', 'lion', 'lizard', 'lobster', 'man', 'maple_tree', 'motorcycle', 'mountain', 'mouse', 'mushroom', 'oak_tree', 'orange', 'orchid', 'otter', 'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+
+
+criterion = nn.CrossEntropyLoss()
+net = BaseNet()
+if IS_GPU:
+    net = net.cuda()
+
+model_name = 'checkPoints/model_99.pth'
+net.load_state_dict(torch.load(model_name))
+net.eval()
+start = time.time()
+
+running_loss = 0.0
+
+
+with open("submission.csv","a") as file_csv: 
+    file_csv.write("pred_label\n")   
+    for i, data in enumerate(testloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        if IS_GPU:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+
+        # wrap them in Variable
+        inputs, labels = Variable(inputs), Variable(labels)
+        # forward + backward + optimize
+        outputs = net(inputs)
+        predictions = torch.softmax(outputs,dim=1)
+        _,predicted = torch.max(predictions,dim=1)
+        predicted =predicted.detach().cpu().numpy()
+        np.savetxt(file_csv,predicted.astype(int),fmt='%i',delimiter='\n')
+        print ('statistics')
+    
+    
+    # Normalizing the loss by the total number of train batches
+
+    
+print(time.time()-start)
+
